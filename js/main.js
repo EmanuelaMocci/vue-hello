baa84eb016f76029703019cfceb173bf4b93a6d5
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-16 15:34:51 +0200

    Aggiunta testo e immagine

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1, 
+// utilizzando i data.
+
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+var app = new Vue({
+        el: '#root',
+        data: {
+            message: 'Hello Vue',
+            image: 'https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg'
+        }
+})
